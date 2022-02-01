3bb738c516e7aff38e029431389e701c590dd3e2
Valeria Mateo Estrada <vmateo@lcg.unam.mx>
2022-02-01 12:46:59 -0600

    Initial commit

---
+++ 01_notas.R
@@ -0,0 +1,26 @@
+library("sessioninfo")
+library("here")
+library("ggplot2")
+
+## Hello world
+print("Soy Leo")
+
+## Directorios
+dir_plots <- here::here("figuras")
+dir_rdata <- here::here("processed-data")
+
+## Crear directorio para las figuras y archivos
+dir.create(dir_plots, showWarnings = FALSE)
+dir.create(dir_rdata, showWarnings = FALSE)
+
+## Hacer una imagen de ejemplo
+pdf(file.path(dir_plots, "mtcars_gear_vs_mpg.pdf"),
+    useDingbats = FALSE
+)
+ggplot(mtcars, aes(group = gear, y = mpg)) +
+  geom_boxplot()
+dev.off()
+
+## Para reproducir mi código
+options(width = 120)
+sessioninfo::session_info()
