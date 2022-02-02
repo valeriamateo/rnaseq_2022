dcb5a27a75afe75039ef5b19de8a07c585a1e448
Valeria Mateo Estrada <vmateo@lcg.unam.mx>
2022-02-02 09:39:29 -0600

    new_script

---
+++ R/02-SummarizedExperiments.R
@@ -0,0 +1,35 @@
+## Lets build our first SummarizedExperiment object
+library("SummarizedExperiment")
+## ?SummarizedExperiment
+
+## De los ejemplos en la ayuda oficial
+
+## Creamos los datos para nuestro objeto de tipo SummarizedExperiment
+## para 200 genes a lo largo de 6 muestras
+nrows <- 200
+ncols <- 6
+## Números al azar de cuentas
+set.seed(20210223)
+counts <- matrix(runif(nrows * ncols, 1, 1e4), nrows)
+## Información de nuestros genes
+rowRanges <- GRanges(
+  rep(c("chr1", "chr2"), c(50, 150)),
+  IRanges(floor(runif(200, 1e5, 1e6)), width = 100),
+  strand = sample(c("+", "-"), 200, TRUE),
+  feature_id = sprintf("ID%03d", 1:200)
+)
+names(rowRanges) <- paste0("gene_", seq_len(length(rowRanges)))
+## Información de nuestras muestras
+colData <- DataFrame(
+  Treatment = rep(c("ChIP", "Input"), 3),
+  row.names = LETTERS[1:6]
+)
+## Juntamos ahora toda la información en un solo objeto de R
+rse <- SummarizedExperiment(
+  assays = SimpleList(counts = counts),
+  rowRanges = rowRanges,
+  colData = colData
+)
+
+## Exploremos el objeto resultante
+rse
